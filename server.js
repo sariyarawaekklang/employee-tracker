58899f219642af091997868bed187f16555b19da
sariyarawaekklang <sariyarawaekklang@gmail.com>
2022-01-30 21:22:53 -0600

    created user prompt

--- server.js
+++ server.js
@@ -3,5 +3,22 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 const db = require('./db/connection');
 
-
-
+// questions for user input
+const questions = () => {
+    return inquirer.prompt([
+    {
+        type: 'list',
+        name: 'options',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Departments',
+            'View All Jobs',
+            'View All Employees',
+            'Add a Department',
+            'Add a Job',
+            'Add an Employee',
+            'Update an Employee Job'
+        ]
+    }
+    ]);
+};
