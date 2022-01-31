b4dba2b68e517846e8f28ffb584c40453ce6b242
sariyarawaekklang <sariyarawaekklang@gmail.com>
2022-01-30 21:57:44 -0600

    created answers

--- server.js
+++ server.js
@@ -17,8 +17,38 @@ const questions = () => {
             'Add a Department',
             'Add a Job',
             'Add an Employee',
-            'Update an Employee Job'
+            'Update an Employee Job',
+            'Exit'
         ]
     }
-    ]);
+    ])
+    .then((answer) => {
+        const { questions } = answer;
+        if (questions === 'View All Departments') {
+
+        }
+        if (questions === 'View All Jobs') {
+
+        }
+        if (questions === 'View All Employees') {
+
+        }
+        if (questions === 'Add a Department') {
+
+        }
+        if (questions === 'Add a Job') {
+
+        }
+        if (questions === 'Add an Employee') {
+
+        }
+        if (questions === 'Update an Employee Job') {
+
+        }
+        if (questions === 'Exit') {
+            console.log('Thank you for using Employee Tracker!');
+        }
+    });
 };
+
+questions();
